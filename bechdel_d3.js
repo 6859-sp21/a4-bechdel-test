2aa4392c2956e162e80fb0dc48df8677c3fc108d
Tanya Yang <tanyang@mit.edu>
2021-04-05 16:35:47 -0400

    radial bar chart eperimentationn

--- bechdel_d3.js
+++ bechdel_d3.js
@@ -22,7 +22,7 @@ d3.csv("https://raw.githubusercontent.com/6859-sp21/a4-bechdel-test/main/movies.
   });
 
   make_stats(all_data);
-  make_plot(activeData)
+  make_plot(all_data.slice(1000))
 
 });
 
@@ -75,66 +75,54 @@ function make_stats(data) {
 }
 
 function make_plot(data) {
-  // NOT SUNBURST -> RADIAL STACKED BAR CHART
-  // need to restructure dataset to be hierarchical (if rating is 3, create all children up to rating?)
-    // need helper function 
-    // array of objects, mapping movie name to bechdel test score at the very least
 
-  // should only be passing in active data (for movies we're interested in querying)
-  // adapted from https://bl.ocks.org/denjn5/e1cdbbe586ac31747b4a304f8f86efa5
-  const width = 1000;
-  const height = 1000;
+  
+  const width = 500;
+  const height = 500;
   const outerRadius = Math.min(width / height) / 2;
   const innerRadius = 200;
 
-  const color = d3.scaleOrdinal(d3.schemeCategory10);
+  const margin = ({top: 10, right: 10, bottom: 20, left: 20});
 
-  // let svg = d3.select("#vis")
-  //   .append("svg")
-  //   .attr("width", width)
-  //   .attr("height", height)
-  //   .append("g")
-  //   .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
-
-  // let barScale = d3.scale.linear()
-  //   .domain([0, data.length])
-  //   .range([0, 3]);
-  
-  y = d3.scaleRadial()
-    // .domain([0, 3])
-    .domain([0, d3.max(data, d => d.rating)])
-    .range([innerRadius, outerRadius])
-  
   x = d3.scaleBand()
     .domain(data.map(d => d.title))
-    .range([0, 2* Math.PI])
+    .range([0, 2 * Math.PI])
     .align(0)
+
+  y = d3.scaleRadial()
+      .domain([0, d3.max(data, d => d.rating)])
+      .range([innerRadius, outerRadius])
   
   arc = d3.arc()
-    .innerRadius(d => y(d[0]))
-    .outerRadius(d => y(d[1]))
-    .startAngle(d => x(d.data.title))
-    .endAngle(d => x(d.data.title) + x.bandwidth())
-    .padAngle(0.01)
-    .padRadius(innerRadius)
+      .innerRadius(innerRadius)
+      .outerRadius(d => parseInt(d.rating))
+      .startAngle(d => x(d.title))
+      .endAngle(d => x(d.title) + x.bandwidth)
+      .padAngle(0.01)
+      .padRadius(innerRadius)
   
-  const svg = d3.select("#vis")
-      .attr("viewBox", `${-width / 2} ${-height / 2} ${width} ${height}`)
+  const svg = d3.select("div#vis")
+      .attr("viewBox", `${width / 2} ${height / 2} ${width} ${height}`)
       .style("width", "100%")
       .style("height", "auto")
       .style("font", "10px sans-serif");
 
+
   svg.append("g")
-    .selectAll("g")
-    .data(data)
-    .join("g")
-      // .attr("fill", d => z(d.key))
     .selectAll("path")
-    .data(d => d)
-    .join("path")
-      .attr("d", arc);
+    .data(data)
+    .enter()
+    .append("path")
+      .attr("fill", "#69b3a2")
+      .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+        .innerRadius(innerRadius)
+        .outerRadius(d => parseInt(d.rating))
+        .startAngle(d => x(d.title))
+        .endAngle(d => x(d.title) + x.bandwidth())
+        .padAngle(0.01)
+        .padRadius(innerRadius))
+
   console.log(svg.node())
-  return svg.node();
   
 }
   
