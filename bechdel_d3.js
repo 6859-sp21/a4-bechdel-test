9a2afa0951962f96494f2154e0c31e8f58560562
Tanya Yang <tanyang@mit.edu>
2021-04-12 20:36:26 -0400

    added text for html

--- bechdel_d3.js
+++ bechdel_d3.js
@@ -108,7 +108,8 @@ function make_plot(data, num) {
   const margin = 200;
   const colorScale = d3.scaleOrdinal()
     .domain([0, 1, 2, 3])
-    .range(d3.schemeTableau10)
+    .range(["#fdbf6f", '#b2df8a', "#fb9a99", "#a6cee3"])
+    
 
   // set the dimensions and margins of the graph
   var width = 1000,
@@ -164,7 +165,7 @@ function make_plot(data, num) {
     .outerRadius(d => y(parseInt(d.rating)))
     .startAngle(d => x(`${d.title} (${d.year}`))
     .endAngle(d => x(`${d.title} (${d.year}`) + x.bandwidth())
-    .padAngle(0.03)
+    .padAngle(0.01)
     .padRadius(innerRadius)
 
   function arcTween(a) {
@@ -223,7 +224,7 @@ function make_plot(data, num) {
       .style("visibility", "hidden")
       .style("background", "white")
       .style("padding", "10px")
-      .style("border-radius", "10px")
+      .style("border-radius", "5px")
       .style("width", "200px")
 
   svg.selectAll("path")
@@ -246,28 +247,12 @@ function make_plot(data, num) {
       d3.select(this)
         .style("opacity", 1.0)})
 
-  // add movie titles
-  // svg.append("g")
-  //     .selectAll("g")
-  //     .data(data)
-  //     .enter()
-  //     .append("g")
-  //       .attr("text-anchor", function(d) { return (x(d.title) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
-  //       .attr("transform", function(d) { return "rotate(" + ((x(d.title) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d.rating)+10) + ",0)"; })
-  //     .append("text")
-  //       .text(function(d){return(d.title)})
-  //       .attr("transform", function(d) { return (x(d.title) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
-  //       .style("font-size", "14px")
-  //       .attr("alignment-baseline", "middle")
-
     // add y axis labels
     svg.append("g")
       .call(yAxis)
 }
 
 function sortData() {
-  // console.log(document.getElementById('sort-data').checked)
-  // if (document.getElementById('sort-data').checked) { // sort
   if (!sorted) { // sort
     unsortedData = [...currentData];
     sorted = true;
@@ -279,8 +264,6 @@ function sortData() {
     currentData = unsortedData
     sorted=false;
     make_plot(unsortedData, 0)
-
-
   }
 }
 
